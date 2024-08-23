b04a5ae4f9e8bc090a551d04ea149afc3e42cc97
Raushankumar saw <raushankumarsaw15@gmail.com>
2024-08-23 11:13:56 +0530

    small bug fixed

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,27 +4,34 @@ import NoItem from "./NoItem";
 import ListItem from "./ListItem";
 function Main() {
   const itemList = useListStorage();
-  const [shortBy,setShortBy]=useState('input')
+  const [shortBy, setShortBy] = useState("input");
 
- let sortedItems=itemList.AllItems;
+  let sortedItems = itemList.AllItems;
 
+  if (shortBy === "item") {
+    sortedItems = itemList.AllItems.slice().sort((a, b) =>
+      a.item.localeCompare(b.item)
+    );
+  }
 
-   if(shortBy==='item'){
-    console.log('yes this is in the description');
-    
-      sortedItems=itemList.AllItems.slice().sort((a,b)=>a.item.localeCompare(b.item))
-   }
-
-   if(shortBy==='Quantity'){
-      sortedItems=itemList.AllItems.slice().sort((a,b)=>a.Quantity-b.Quantity  )
-   }
+  if (shortBy === "Quantity") {
+    sortedItems = itemList.AllItems.slice().sort(
+      (a, b) => a.Quantity - b.Quantity
+    );
+  }
   function handleChecked(id) {
     itemList.CheckedHandler(id);
   }
 
   let content =
     itemList.AllItems.length > 0 ? (
-      <ListItem items={sortedItems} onchange={handleChecked}  onDelete={(id)=>{itemList.deleteHandler(id)}}/>
+      <ListItem
+        items={sortedItems}
+        onchange={handleChecked}
+        onDelete={(id) => {
+          itemList.deleteHandler(id);
+        }}
+      />
     ) : (
       <NoItem />
     );
@@ -32,7 +39,13 @@ function Main() {
   return (
     <main className="min-h-[64vh] md:h-[78vh] lg:h-[69.2vh] relative bg-neutral-700 flex flex-col flex-grow-1 w-full">
       {content}
-      <select name="sortBy" id="shortItem" className="text-center absolute bottom-2 md:right-[17.5rem]  right-[6rem] lg:right-[41em] py-2 rounded-full bg-yellow-200 "  value={shortBy} onChange={(e)=>setShortBy(e.target.value)}>
+      <select
+        name="sortBy"
+        id="shortItem"
+        className="text-center absolute bottom-2 md:right-[17.5rem]  right-[6rem] lg:right-[41em] py-2 rounded-full bg-yellow-200 "
+        value={shortBy}
+        onChange={(e) => setShortBy(e.target.value)}
+      >
         <option value="input">short by Input</option>
         <option value="item">short by the item</option>
         <option value="Quantity">short by the Quantity</option>
